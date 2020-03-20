f10812f81d65d3a9826a5954888b12824d124f26
Kirin <1370302499@qq.com>
2020-03-21 01:13:05 +0800

    luaapi.h加了注册C-API函数的宏

--- src_cpp/include/luaapi.h
+++ src_cpp/include/luaapi.h
@@ -7,4 +7,15 @@ extern "C" {
 #include <lualib.h>
 }
 
-#endif
+// register C-API functions
+// usage: LUA_REG_FUNC(library,C_API(function1),C_API(function2),...)
+#define C_API(function) \
+    { #function, function }
+#define LUA_REG_FUNC(library, ...)                                 \
+    extern "C" int luaopen_lib_##library(lua_State *lua_state) {   \
+        static constexpr luaL_reg FUNCTIONS[] = {__VA_ARGS__, {}}; \
+        luaL_register(lua_state, #library, FUNCTIONS);             \
+        return 1;                                                  \
+    }
+
+#endif  //__LUAAPI_H__
