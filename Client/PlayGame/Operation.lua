3ecd05ef90fb5cb3a0360c7912212c423ac5e361
wuzirui <wuzirui@tztyun.org>
2020-04-27 18:29:12 +0800

    判断两个格子是否相邻&移动

--- Client/PlayGame/Operation.lua
+++ Client/PlayGame/Operation.lua
@@ -34,33 +34,48 @@ function Operation.Select(x, y)
     end
 end
 
+function Operation.IsConnected(posX1, posY1, posX2, posY2)
+    if posX1 == posX2 then
+        if posY1 - posY2 == 1 or posY2 - posY1 == 1 then
+            return true
+        end
+    end
+
+    if posX1 % 2 == 1 then
+        if (posX1 == posX2 + 1 or posX1 == posX2 - 1) and (posY1 == posY2 or posY1 == posY2 - 1) then return true end
+    else
+        if (posX1 == posX2 + 1 or posX1 == posX2 - 1) and (posY1 == posY2 or posY1 == posY2 + 1) then return true end
+    end
+    return false
+end
+
 function Operation.MoveTo(x, y)
-    local absDetX = math.abs(x - Operation.SelectPos.x)
-    local absDetY = math.abs(y - Operation.SelectPos.y)
-    -- 这里就是判断是否是四联通，如果括号内为真，
-    -- 说明和所选中格子(即Operation.SelectPos)是四联通关系
-    -- 否则就不执行MoveTo
-    if not (absDetX * absDetY == 0 and absDetX + absDetY == 1) then
+
+    if not Operation.IsConnected(Operation.SelectPos.x, Operation.SelectPos.y, x, y) then
         return
     end
+    print("Connected")
     Operation.Queue:Push(
-        {
-            armyID = PlayGame.armyID,
-            srcX = Operation.SelectPos.x,
-            srcY = Operation.SelectPos.y,
-            dstX = x,
-            dstY = y
-        }
+    {
+        armyID = PlayGame.armyID,
+        srcX = Operation.SelectPos.x,
+        srcY = Operation.SelectPos.y,
+        dstX = x,
+        dstY = y
+    }
     )
 end
 
 function Operation.CatchMousePressed(pixelX, pixelY, button, istouch, presses)
     -- 鼠标坐标转换为地图坐标
     local x, y = BasicMap.Pixel2Coordinate(pixelX, pixelY)
+    print("mouse_pressed")
+
     -- 说明鼠标点的位置不在地图中
     if x == -1 and y == -1 then
         return
     end
+    print(string.format("Chosen point %d %d", x, y))
     if Operation.SelectPos == nil then -- 没有选择的情况下要选择
         Operation.Select(x, y)
     else -- 选择后的情况要移动
@@ -72,8 +87,7 @@ function Operation.DrawSelect()
     if Operation.SelectPos == nil then
         return
     end
-    local pixelX, pixelY =
-        BasicMap.Coordinate2Pixel(Operation.SelectPos.x, Operation.SelectPos.y)
+    local pixelX, pixelY =    BasicMap.Coordinate2Pixel(Operation.SelectPos.x, Operation.SelectPos.y)
     Picture.DrawSelect(pixelX, pixelY)
 end
 
