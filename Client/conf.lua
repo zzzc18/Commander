10370df051d1387df397bc17b962f6bc2f4cf883
wuzirui <wuzirui@tztyun.org>
2020-11-20 18:48:57 +0800

    窗口可变大小

--- Client/conf.lua
+++ Client/conf.lua
@@ -1,6 +1,8 @@
 function love.conf(this)
     -- this.window.width = 1920
     -- this.window.height = 1012
+    -- this.window.setMode({ resizable = true })
+    this.window.resizable = true
     this.modules.joystick = false
     this.modules.physics = false
 end
