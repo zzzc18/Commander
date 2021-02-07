44ec1aa2dbd409f9327a3d8c59f925e6b3306c6c
unknown <45946110@qq.com>
2021-02-07 14:34:01 +0800

    优化了Buttons.lua

--- System/Buttons.lua
+++ System/Buttons.lua
@@ -1,71 +1,38 @@
 Buttons = {}
 
-Buttons.ButtonsNum = 5
-
 EachButton = {}
+ButtonsBasic = {}
 
 IsPause = false
 
 function Buttons.Init()
-    EachButton:Load()
+    ButtonsBasic:Load()
 end
 
-function EachButton.NewButton(path, name, x, y, ratio)
+function ButtonsBasic:NewButton(path, name, x, y)
     local button = {}
     button.imag = love.graphics.newImage(path)
     button.name = name
     button.x = x
     button.y = y
-    button.ratio = ratio
-    button.offset = 25
+    button.ratio = self.ratio
+    button.offset = -20
     button.scalingcenterX = 0
     button.scalingcenterY = 0
-    button.diaphaneity = EachButton.initialDiaphaneity
+    button.diaphaneity = self.initialDiaphaneity
     return button
 end
 
-function EachButton:Load()
+function ButtonsBasic:Load()
     local windowWidth, windowHeight = love.graphics.getDimensions()
-    self[1] =
-        EachButton.NewButton(
-        "data/Picture/BUTTON_TYPE_LAST.png",
-        "last",
-        windowWidth * 0.35,
-        windowHeight * 0,
-        self.initialRatio
-    )
-    self[2] =
-        EachButton.NewButton(
-        "data/Picture/BUTTON_TYPE_PAUSE.png",
-        "pause",
-        windowWidth * 0.45,
-        windowHeight * 0,
-        self.initialRatio
-    )
-    self[3] =
-        EachButton.NewButton(
-        "data/Picture/BUTTON_TYPE_CONTINUE.png",
-        "continue",
-        windowWidth * 0.45,
-        windowHeight * 0,
-        self.initialRatio
-    )
-    self[4] =
-        EachButton.NewButton(
-        "data/Picture/BUTTON_TYPE_NEXT.png",
-        "next",
-        windowWidth * 0.55,
-        windowHeight * 0,
-        self.initialRatio
-    )
-    self[5] =
-        EachButton.NewButton(
-        "data/Picture/BUTTON_TYPE_SHIFTSPEED.png",
-        "shift_speed",
-        windowWidth * 0.65,
-        windowHeight * 0,
-        self.initialRatio
-    )
+    EachButton.Last = ButtonsBasic:NewButton("data/Picture/BUTTON_TYPE_LAST.png", "last", windowWidth * 0.35, windowHeight * 0)
+    EachButton.Pause =
+        ButtonsBasic:NewButton("data/Picture/BUTTON_TYPE_PAUSE.png", "pause", windowWidth * 0.45, windowHeight * 0)
+    EachButton.Continue =
+        ButtonsBasic:NewButton("data/Picture/BUTTON_TYPE_CONTINUE.png", "continue", windowWidth * 0.45, windowHeight * 0)
+    EachButton.Next = ButtonsBasic:NewButton("data/Picture/BUTTON_TYPE_NEXT.png", "next", windowWidth * 0.55, windowHeight * 0)
+    EachButton.ShiftSpeed =
+        ButtonsBasic:NewButton("data/Picture/BUTTON_TYPE_SHIFTSPEED.png", "shiftSpeed", windowWidth * 0.65, windowHeight * 0)
     self.initialRatio = 0.2
     self.laterRatio = 0.25
     self.initialDiaphaneity = 0.5
@@ -73,23 +40,23 @@ function EachButton:Load()
 end
 
 function Buttons.DrawButtons()
-    for i = 1, Buttons.ButtonsNum do
-        if true == IsPause and 2 == i or false == IsPause and 3 == i then
+    for i, button in pairs(EachButton) do
+        if true == IsPause and "pause" == button.name or false == IsPause and "continue" == button.name then
         else
-            love.graphics.setColor(1, 1, 1, EachButton[i].diaphaneity)
+            love.graphics.setColor(1, 1, 1, button.diaphaneity)
             love.graphics.draw(
-                EachButton[i].imag,
-                EachButton[i].x,
-                EachButton[i].y,
+                button.imag,
+                button.x,
+                button.y,
                 0,
-                EachButton[i].ratio,
-                EachButton[i].ratio,
-                EachButton[i].offset,
-                -EachButton[i].offset,
-                EachButton[i].scalingcenterX,
-                EachButton[i].scalingcenterY
+                button.ratio,
+                button.ratio,
+                button.offset,
+                button.offset,
+                button.scalingcenterX,
+                button.scalingcenterY
             )
-            EachButton[i].ratio = EachButton.initialRatio
+            button.ratio = ButtonsBasic.initialRatio
         end
     end
 end
@@ -98,68 +65,64 @@ end
 function Buttons.MouseState(mouseX, mouseY, mode)
     local name = nil
     local inButton = false
-    for i = 1, Buttons.ButtonsNum do
-        if 2 == i and true == IsPause then
-            i = i + 1
-        end
-        if 3 == i and false == IsPause then
-            i = i + 1
-        end
-        if
-            mouseX > EachButton[i].x and mouseX < EachButton[i].x + 50 * love.graphics.getWidth() / 1080 and
-                mouseY > EachButton[i].y and
-                mouseY < EachButton[i].y + 60 * love.graphics.getWidth() / 1080
-         then
-            inButton = true
-            if 0 == mode then
-                EachButton[i].diaphaneity = EachButton.laterDiaphaneity
-                name = "readyToClick"
-                break
-            elseif 1 == mode then
-                EachButton:MouseSuspension(EachButton[i])
-                break
-            elseif 2 == mode then
-                name = EachButton:ButtonsRelease(EachButton[i])
-                break
+    for i, button in pairs(EachButton) do
+        if true == IsPause and "pause" == button.name or false == IsPause and "continue" == button.name then
+        else
+            if
+                mouseX > button.x and mouseX < button.x + 63 * love.graphics.getWidth() / 1080 and mouseY > button.y and
+                    mouseY < button.y + 63 * love.graphics.getWidth() / 1080
+             then
+                inButton = true
+                if 0 == mode then
+                    button.diaphaneity = ButtonsBasic.laterDiaphaneity
+                    name = "readyToClick"
+                    break
+                elseif 1 == mode then
+                    ButtonsBasic:MouseSuspension(button)
+                    break
+                elseif 2 == mode then
+                    name = ButtonsBasic:ButtonsRelease(button)
+                    break
+                end
             end
         end
     end
     if not inButton then
-        EachButton:CleanAll()
+        ButtonsBasic:CleanAll()
     end
     return name
 end
 
-function EachButton:CleanAll()
-    for i = 1, Buttons.ButtonsNum do
-        EachButton[i].diaphaneity = EachButton.initialDiaphaneity
+function ButtonsBasic:CleanAll()
+    for i, button in pairs(EachButton) do
+        button.diaphaneity = self.initialDiaphaneity
     end
 end
 
-function EachButton:ButtonsRelease(button)
-    button.diaphaneity = EachButton.initialDiaphaneity
-    if button == EachButton[2] then
+function ButtonsBasic:ButtonsRelease(button)
+    button.diaphaneity = self.initialDiaphaneity
+    if button == EachButton.Pause then
         IsPause = true
-    elseif button == EachButton[3] then
+    elseif button == EachButton.Continue then
         IsPause = false
     end
     print(button.name)
     return button.name
 end
 
-function EachButton:MouseSuspension(button)
-    button.ratio = EachButton.laterRatio * love.graphics.getWidth() / 1080
+function ButtonsBasic:MouseSuspension(button)
+    button.ratio = self.laterRatio * love.graphics.getWidth() / 1080
 end
 
 function Buttons.Update()
-    local windowWidth, windowHeight = love.graphics.getDimensions()
-    EachButton[1].x = windowWidth * 0.35
-    EachButton[2].x = windowWidth * 0.45
-    EachButton[3].x = windowWidth * 0.45
-    EachButton[4].x = windowWidth * 0.55
-    EachButton[5].x = windowWidth * 0.65
-    for i = 1, Buttons.ButtonsNum do
-        EachButton[i].ratio = EachButton.initialRatio * windowWidth / 1080
+    local windowWidth = love.graphics.getWidth()
+    EachButton.Last.x = windowWidth * 0.35
+    EachButton.Pause.x = windowWidth * 0.45
+    EachButton.Continue.x = windowWidth * 0.45
+    EachButton.Next.x = windowWidth * 0.55
+    EachButton.ShiftSpeed.x = windowWidth * 0.65
+    for i, button in pairs(EachButton) do
+        button.ratio = ButtonsBasic.initialRatio * windowWidth / 1080
     end
     Buttons.MouseState(love.mouse.getX(), love.mouse.getY(), 1)
 end
