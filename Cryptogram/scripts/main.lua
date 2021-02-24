b781a23c4c38827278e3d6730c1fbe0957a6358e
Sumorile <474743988@qq.com>
2021-02-24 21:21:22 +0800

    加了一点注释

--- Cryptogram/scripts/main.lua
+++ Cryptogram/scripts/main.lua
@@ -1,11 +1,16 @@
 operators = require("libraries.operators")
 
+-- 要加密的字符串A
 A = "cnsdjncjsdncjsd"
+
+-- 加密后的长度，应为128
 Len = 0
 while Len ~= 128 do
+    -- 加密后的字符串B
     B = operators.encrypt(A)
     Len = string.len(B)
 end
 print(B)
+-- 解密B之后得到C，yuA一致
 C = operators.decrypt(B)
 print(C)
