1f44fc60cc50aee2d44925df990d531aa8b88001
SethArgent <1920976618@qq.com>
2021-03-04 18:45:07 +0800

    实现记录从王到指定点的路径的功能，在此基础上实现了让下辖军队返回王的位置，以及从王的位置进攻敌人的王的功能。目前仍存在BUG

--- Client/AI/AI_SDK/AI_SDK.lua
+++ Client/AI/AI_SDK/AI_SDK.lua
@@ -12,8 +12,13 @@ AI_SDK.judgementState = "Running"
 AI_SDK.armyID = nil
 AI_SDK.armyNum = 0
 AI_SDK.KingPos = {x = -1, y = -1}
+-- 记录每个从王到每个点的路径，每个元素包含两个table和一个字符串
+-- pos记录坐标，commands记录路径，type记录类型
+AI_SDK.Unit = {}
 
 local timer = 0
+local TempCommand = {}
+local TempCommandLength = 0
 
 function AI_SDK.Init()
     AI_SDK.gameState = "READY"
@@ -68,7 +73,6 @@ end
 function AI_SDK.keyreleased(key, scancode)
 end
 
-
 function AI_SDK.draw()
     if AI_SDK.gameState == "READY" then
         Picture.DrawReady(BGAnimation)
@@ -86,7 +90,7 @@ function AI_SDK.draw()
 end
 
 -- 移动的函数
-function AI_SDK.MoveTo(x, y, moveNum)
+function AI_SDK.MoveTo(x, y, moveNum, dir)
     if AI_SDK.gameState ~= "Start" then
         return
     --只有游戏进行时才能发送
@@ -117,6 +121,14 @@ function AI_SDK.MoveTo(x, y, moveNum)
         num = moveNum
     }
     ClientSock.SendMove(NewRequest)
+    --  记录路径
+    if "NODE_TYPE_KING" == CGameMap.GetNodeType(x, y) then
+        AI_SDK.addCommands({key = "isKing", dir = dir, x = x, y = y, type = "KING"})
+    elseif "NODE_TYPE_BLANK" == CGameMap.GetNodeType(x, y) then
+        AI_SDK.addCommands({key = "Done", dir = dir, x = x, y = y, type = "BLANK"})
+    elseif "NODE_TYPE_FORT" == CGameMap.GetNodeType(x, y) then
+        AI_SDK.addCommands({key = "Done", dir = dir, x = x, y = y, type = "FORT"})
+    end
 end
 
 --检查pos之间是否相邻
@@ -129,17 +141,11 @@ function AI_SDK.IsConnected(posX1, posY1, posX2, posY2)
     end
 
     if posX1 % 2 == 1 then
-        if
-            (posX1 == posX2 + 1 or posX1 == posX2 - 1) and
-                (posY1 == posY2 or posY1 == posY2 - 1)
-         then
+        if (posX1 == posX2 + 1 or posX1 == posX2 - 1) and (posY1 == posY2 or posY1 == posY2 - 1) then
             return true
         end
     else
-        if
-            (posX1 == posX2 + 1 or posX1 == posX2 - 1) and
-                (posY1 == posY2 or posY1 == posY2 + 1)
-         then
+        if (posX1 == posX2 + 1 or posX1 == posX2 - 1) and (posY1 == posY2 or posY1 == posY2 + 1) then
             return true
         end
     end
@@ -149,16 +155,106 @@ end
 function AI_SDK.UpdateTimerSecond(dt)
 end
 
-function AI_SDK.update(dt)
-    if AI_SDK.gameState == "READY" then
-        BGAnimation.update(dt)
+-- 向Unit中添加路径
+function AI_SDK.addCommands(data)
+    if data.key == nil then
+        AI_SDK.clearCommands()
+    elseif data.key == "Done" then
+        local isRepeated = false
+        TempCommandLength = TempCommandLength + 1
+        TempCommand[TempCommandLength] = data.dir
+        for i, unit in pairs(AI_SDK.Unit) do
+            if AI_SDK.armyID ~= CGameMap.GetBelong(unit.pos.x, unit.pos.y) and unit.type ~= "KING" then
+                -- 移除无效路径
+                table.remove(AI_SDK.Unit, i)
+            elseif unit.pos.x == data.x and unit.pos.y == data.y then
+                if #unit.commands > TempCommandLength then
+                    -- 当前记录的路径更短,覆盖原有路径
+                    unit.commands = AI_SDK.reverseTable(TempCommand)
+                else
+                    if #unit.commands < TempCommandLength then
+                        -- 原有路径更短，覆盖暂存路径
+                        TempCommand = AI_SDK.deepCopy(unit.commands)
+                        TempCommandLength = #unit.commands
+                    end
+                end
+                isRepeated = true
+                break
+            end
+        end
+        if not isRepeated then
+            table.insert(
+                AI_SDK.Unit,
+                1,
+                {pos = {x = data.x, y = data.y}, commands = AI_SDK.reverseTable(TempCommand), type = data.type}
+            )
+        end
+    elseif data.key == "isKing" then
+        if AI_SDK.armyID == CGameMap.GetBelong(data.x, data.y) then
+            AI_SDK.clearCommands()
+        else
+            -- 记录下敌方王的位置，保留正向序列，用于进攻时调用
+            print("Find a king!")
+            local isRepeated = false
+            TempCommandLength = TempCommandLength + 1
+            TempCommand[TempCommandLength] = data.dir
+            for i, unit in pairs(AI_SDK.Unit) do
+                if AI_SDK.armyID == CGameMap.GetBelong(unit.pos.x, unit.pos.y) and unit.type == "KING" then
+                    -- 移除无效路径
+                    table.remove(AI_SDK.Unit, i)
+                elseif unit.pos.x == data.x and unit.pos.y == data.y then
+                    if #unit.commands > TempCommandLength then
+                        -- 当前记录的路径更短,覆盖原有路径
+                        unit.commands = AI_SDK.reverseTable(TempCommand)
+                    else
+                        if #unit.commands < TempCommandLength then
+                            -- 原有路径更短，覆盖暂存路径
+                            TempCommand = AI_SDK.deepCopy(unit.commands)
+                            TempCommandLength = #unit.commands
+                        end
+                    end
+                    isRepeated = true
+                    break
+                end
+            end
+            if not isRepeated then
+                table.insert(
+                    AI_SDK.Unit,
+                    1,
+                    {pos = {x = data.x, y = data.y}, commands = AI_SDK.reverseTable(TempCommand), type = data.type}
+                )
+            end
+        end
     end
-    Client:update()
-    if AI_SDK.gameState ~= "Start" and AI_SDK.gameState ~= "Menu" then
-        return
+end
+
+-- 清空暂存路径
+function AI_SDK.clearCommands()
+    TempCommand = {}
+    TempCommandLength = 0
+end
+
+function AI_SDK.reverseTable(table)
+    if nil == table then
+        return nil
     end
-    MapAdjust.Update()
-    Operation.Update()
+    local reverseTable = {}
+    for j = 1, #table do
+        reverseTable[j] = table[#table - j + 1]
+    end
+    return reverseTable
+end
+
+-- 进行深拷贝
+function AI_SDK.deepCopy(table)
+    if nil == table then
+        return nil
+    end
+    local res = {}
+    for i, v in ipairs(table) do
+        res[i] = v
+    end
+    return res
 end
 
 function AI_SDK.update(dt)
--- Client/AI/Core.lua
+++ Client/AI/Core.lua
@@ -3,8 +3,36 @@ local Core = {}
 Core.SelectPos = {x = -1, y = -1}
 Core.rdmDirection = 0
 
+local BackCommands = {}
+-- Backing:正在撤退
+-- Done:撤退完毕
+local backState = "Done"
+local backIndex = 1
+local backCommandsIndex = 1
+local AttackCommands = {}
+-- Ready:可以进攻
+-- Attacking:正在进攻
+-- Done:进攻完毕
+local attackState = "Done"
+local attackIndex = 1
+
 function Core.Main(data)
-    Core.AIMove_mode1(data)
+    if ReplayGame.step % 45 == 0 and backState ~= "Backing" and attackState == "Done" then
+        Core.BackInit()
+        Core.BackToKing()
+    elseif backState == "Backing" then
+        Core.BackToKing()
+    elseif attackState == "Ready" then
+        print("--Attacking--")
+        attackState = "Attacking"
+        attackIndex = 1
+        Core.SelectPos.x, Core.SelectPos.y = AI_SDK.KingPos.x, AI_SDK.KingPos.y
+        Core.Attack_mode1()
+    elseif attackState == "Attacking" then
+        Core.Attack_mode1()
+    elseif backState == "Done" and attackState == "Done" then
+        Core.AIMove_mode1(data)
+    end
     return
 end
 
@@ -20,6 +48,7 @@ function Core.AIMove_mode1(data)
     if AI_SDK.armyID ~= CGameMap.GetBelong(x, y) or unitNum <= 1 then -- 不移动，并让下次移动从王开始
         Core.SelectPos.x = AI_SDK.KingPos.x
         Core.SelectPos.y = AI_SDK.KingPos.y
+        AI_SDK.clearCommands()
         return
     elseif unitNum >= 50 and "NODE_TYPE_KING" == CGameMap.GetNodeType(x, y) then
         moveNum = 0.5 -- 只移动一半
@@ -32,40 +61,37 @@ function Core.AIMove_mode1(data)
         mode = x % 2 + 1
         x = x + BasicMap.direction[mode][Core.rdmDirection][1]
         y = y + BasicMap.direction[mode][Core.rdmDirection][2]
-        if
-            "NODE_TYPE_BLANK" == CGameMap.GetNodeType(x, y) or
-                "NODE_TYPE_KING" == CGameMap.GetNodeType(x, y)
-         then
+        if "NODE_TYPE_BLANK" == CGameMap.GetNodeType(x, y) or "NODE_TYPE_KING" == CGameMap.GetNodeType(x, y) then
             -- 尽可能地去攻占BLANK和进攻KING
             if AI_SDK.armyID ~= CGameMap.GetBelong(x, y) then
-                AI_SDK.MoveTo(x, y, moveNum)
+                AI_SDK.MoveTo(x, y, moveNum, Core.rdmDirection)
                 Core.SelectPos.x, Core.SelectPos.y = x, y
                 break
             else
                 chance = chance - 1
                 if chance < 3 then
                     -- 向己方地区移动
-                    AI_SDK.MoveTo(x, y, moveNum)
+                    AI_SDK.MoveTo(x, y, moveNum, Core.rdmDirection)
                     Core.SelectPos.x, Core.SelectPos.y = x, y
                     break
                 end
             end
         elseif "NODE_TYPE_FORT" == CGameMap.GetNodeType(x, y) then
             local fortNum = CGameMap.GetUnitNum(x, y)
-            if
-                AI_SDK.armyID ~= CGameMap.GetBelong(x, y) and
-                    fortNum - 10 < unitNum
-             then
+            if AI_SDK.armyID ~= CGameMap.GetBelong(x, y) and fortNum - 10 < unitNum then
                 -- 在兵力足够时选择进攻FORT
-                AI_SDK.MoveTo(x, y, moveNum)
+                AI_SDK.MoveTo(x, y, moveNum, Core.rdmDirection)
                 Core.SelectPos.x, Core.SelectPos.y = x, y
                 break
             else
                 if AI_SDK.armyID == CGameMap.GetBelong(x, y) then
-                    -- 向己方FORT移动
-                    AI_SDK.MoveTo(x, y, moveNum)
-                    Core.SelectPos.x, Core.SelectPos.y = x, y
-                    break
+                    chance = chance - 1
+                    if "NODE_TYPE_FORT" ~= CGameMap.GetNodeType(Core.SelectPos.x, Core.SelectPos.y) or chance < 2 then
+                        -- 向己方FORT移动,但是如果两个FORT相邻则降低优先级
+                        AI_SDK.MoveTo(x, y, moveNum, Core.rdmDirection)
+                        Core.SelectPos.x, Core.SelectPos.y = x, y
+                        break
+                    end
                 else
                     chance = chance - 1
                 end
@@ -78,6 +104,161 @@ function Core.AIMove_mode1(data)
         -- 重新从王的位置开始
         Core.SelectPos.x = AI_SDK.KingPos.x
         Core.SelectPos.y = AI_SDK.KingPos.y
+        AI_SDK.clearCommands()
+    end
+end
+
+-- 为Core.BackToKing()初始化
+function Core.BackInit()
+    backIndex = 1
+    backState = "Backing"
+    -- 导入Unit中记录的路径
+    for i, unit in pairs(AI_SDK.Unit) do
+        if AI_SDK.armyID ~= CGameMap.GetBelong(unit.pos.x, unit.pos.y) then
+            if unit.type == "KING" then
+                print("Find a king, ready to attack!")
+                if attackState ~= "Attacking" then
+                    attackState = "Ready"
+                    table.insert(
+                        AttackCommands,
+                        1,
+                        {
+                            pos = {x = unit.pos.x, y = unit.pos.y},
+                            commands = AI_SDK.reverseTable(unit.commands),
+                            type = unit.type
+                        }
+                    )
+                end
+            else
+                -- 移除无效路径
+                table.remove(AI_SDK.Unit, i)
+            end
+        else
+            if #unit.commands > 12 then
+                -- 路径太长，耽误时间
+            else
+                BackCommands[backIndex] = {
+                    pos = {x = unit.pos.x, y = unit.pos.y},
+                    commands = AI_SDK.deepCopy(unit.commands),
+                    type = unit.type
+                }
+                backIndex = backIndex + 1
+            end
+            if backIndex > 30 then
+                -- 当前撤退算法效率低，故限制最多进行30次撤退，防止AI进行过多低效撤退
+                break
+            end
+        end
+    end
+    backIndex = 1
+    backCommandsIndex = 1
+    Core.SelectPos.x, Core.SelectPos.y = BackCommands[backIndex].pos.x, BackCommands[backIndex].pos.y
+end
+
+-- 让所有兵力撤回到王的位置
+function Core.BackToKing()
+    local x, y, mode, dir
+    if
+        CGameMap.GetUnitNum(Core.SelectPos.x, Core.SelectPos.y) <= 1 or
+            (Core.SelectPos.x == AI_SDK.KingPos.x and Core.SelectPos.y == AI_SDK.KingPos.y)
+     then
+        -- 选择点兵力不足或者选择到了王，则开始下一轮撤退
+        backIndex = backIndex + 1
+        if backIndex > #BackCommands then
+            -- 撤退完毕
+            backState = "Done"
+            print("All back completed! (1)")
+            BackCommands = {}
+        else
+            Core.SelectPos.x, Core.SelectPos.y = BackCommands[backIndex].pos.x, BackCommands[backIndex].pos.y
+            Core.BackToKing()
+        end
+        return
+    else
+        x, y = Core.SelectPos.x, Core.SelectPos.y
+        if BackCommands[backIndex].commands[backCommandsIndex] == nil then
+            -- 报错
+            print("-------------------")
+            print("An error occured: a invalid order!")
+            print(
+                "Information: backCommandsIndex= " ..
+                    backCommandsIndex ..
+                        " and #BackCommands[backIndex].commands = " .. #BackCommands[backIndex].commands
+            )
+            if BackCommands[backIndex].pos ~= nil then
+                print(
+                    "pos.x = " ..
+                        BackCommands[backIndex].pos.x ..
+                            " ,and pos.y = " ..
+                                BackCommands[backIndex].pos.y .. " .The node type = " .. CGameMap.GetNodeType(x, y)
+                )
+            end
+            if BackCommands[backIndex] == nil then
+                print(
+                    "Exactly, backIndex = " ..
+                        backIndex .. " ,but this is a nil value in the table! The #BackCommands = " .. #BackCommands
+                )
+            else
+                print("backIndex = " .. backIndex .. " , and #BackCommands = " .. #BackCommands)
+            end
+            print("-------------------")
+        else
+            if BackCommands[backIndex].commands[backCommandsIndex] <= 3 then
+                dir = BackCommands[backIndex].commands[backCommandsIndex] + 3
+            else
+                dir = BackCommands[backIndex].commands[backCommandsIndex] - 3
+            end
+            mode = x % 2 + 1
+            x = x + BasicMap.direction[mode][dir][1]
+            y = y + BasicMap.direction[mode][dir][2]
+            AI_SDK.MoveTo(x, y, 0)
+            Core.SelectPos.x, Core.SelectPos.y = x, y
+        end
+        backCommandsIndex = backCommandsIndex + 1
+        if backCommandsIndex > #BackCommands[backIndex].commands then
+            backIndex = backIndex + 1
+            backCommandsIndex = 1
+            if backIndex > #BackCommands then
+                -- 撤退完毕
+                backIndex = 1
+                backState = "Done"
+                print("All back completed! (2)")
+                BackCommands = {}
+                return
+            end
+            Core.SelectPos.x, Core.SelectPos.y = BackCommands[backIndex].pos.x, BackCommands[backIndex].pos.y
+        end
+    end
+end
+
+-- 让AI从王的位置向另一个王进攻
+function Core.Attack_mode1()
+    if AttackCommands == nil or AttackCommands[1] == nil then
+        print("Error: invalid attack command!")
+        attackState = "Done"
+        AttackCommands = {}
+        return
+    end
+    local x, y, mode, dir
+    dir = AttackCommands[1].commands[attackIndex]
+    if dir == nil then
+        print("Error: an invalid attackIndex!")
+        attackState = "Done"
+        AttackCommands = {}
+        return
+    end
+    x, y = Core.SelectPos.x, Core.SelectPos.y
+    mode = x % 2 + 1
+    x = x + BasicMap.direction[mode][dir][1]
+    y = y + BasicMap.direction[mode][dir][2]
+    AI_SDK.MoveTo(x, y, 0)
+    Core.SelectPos.x, Core.SelectPos.y = x, y
+    attackIndex = attackIndex + 1
+    if attackIndex > #AttackCommands[1].commands then
+        print("Finished an attack!")
+        attackState = "Done"
+        AttackCommands = {}
+        return
     end
 end
 
