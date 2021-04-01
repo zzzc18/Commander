35a752210d7fa58e5fd8b12e9cb439676d2e9b80
wuzirui <wuzirui@tztyun.org>
2021-04-01 12:25:35 +0800

    Update API & Implementation

--- src_cpp/UserImplementation/API.cpp
+++ src_cpp/UserImplementation/API.cpp
@@ -1,9 +1,27 @@
 #include "LuaAPI.hpp"
 #include "UserAPI.hpp"
 
+enum back_state_type { Backing, Done } back_state;
+enum attack_state_type { Done, Ready, Attacking } attack_state;
+
+void Back_init();
+void Back_to_king();
+
+int attack_index = 0;
+
 static int userMain(lua_State *luaState) {
     printf("C++ Implementation Invoke\n");
-    
+
+    // 初始化API单例
+    UserAPI &API = UserAPI.Singleton(luaState);
+
+    if (API.get_current_step() % 45 == 0 &&
+        back_state != back_state_type::Backing attack_state ==
+            attack_state_type::Done) {
+        Back_init();
+        Back_to_king();
+    }
+
     return 0;
 }
 
--- src_cpp/include/UserAPI.hpp
+++ src_cpp/include/UserAPI.hpp
@@ -6,7 +6,6 @@
 #include "GameMap.hpp"
 #include "LuaAPI.hpp"
 
-
 class UserAPI {
    public:
     void move_to(int x, int y, double moveNum, int direction);
@@ -19,14 +18,20 @@ class UserAPI {
     std::string get_judgement_state() const;
     int get_armyID() const;
     int get_army_num() const;
-    VECTOR get_king_pos() const;
-    static UserAPI & Singleton(lua_State * L = nullptr);
+    VECTOR king_pos() const;
     static bool has_init;
+    int get_current_step() const;
+    VECTOR selected_pos() const;
+    void selected_pos(VECTOR new_pos);
+    int get_unit_num(VECTOR pos) const;
+
     UserAPI(const UserAPI &) = delete;
-    bool operator =(const UserAPI &) = delete;
-private:
-    mutable lua_State* luaState;
-    UserAPI(lua_State * L): luaState(L) {}
+    bool operator=(const UserAPI &) = delete;
+    static UserAPI &Singleton(lua_State *L = nullptr);
+
+   private:
+    mutable lua_State *luaState;
+    UserAPI(lua_State *L) : luaState(L) {}
     void init_func_call(std::string class_name, std::string func_name);
     void execute_func_call(int param_num = 0, int ret_num = 0);
     void lua_pushstring(std::string str);
@@ -35,18 +40,4 @@ private:
 
 bool UserAPI::has_init = false;
 
-
-
 #endif  // UserAPI_hpp
-
-#ifndef UserCore_hpp
-#define UserCore_hpp
-
-class UserCore {
-public:
-    int get_current_step() const; 
-    VECTOR get_select_pos() const;
-    
-};
-
-#endif  // UserCore_hpp
