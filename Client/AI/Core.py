76a9ecccef2f0e407b5307184b06114043dadbfc
zzzc18 <zzzc18@outlook.com>
2021-04-30 00:09:27 +0800

    添加了Pyi文件，方便Python接口自动补全。拆分Sample和Core

--- Client/AI/Core.py
+++ Client/AI/Core.py
@@ -1,35 +1,15 @@
-from Commander import AI_SDK, GameMap, Verification
-from random import randint
-
-armyID, mapX, mapY = 0, 0, 0
-
-
-def RandomSelect():
-    controllingArea = []
-    for i in range(mapX):
-        for j in range(mapY):
-            if GameMap.GetBelong(i, j) == armyID:
-                controllingArea.append([i, j])
-
-    u = randint(0, len(controllingArea)-1)
-    direction = randint(0, 5)
-    return controllingArea[u][0], controllingArea[u][1], direction
+from Commander import AI_SDK, GameMap, Verification, NODE_TYPE
 
 
 def Load():
     '''
     开始时调用
     '''
-    global armyID, mapX, mapY
-    armyID = Verification.GetArmyID()
-    mapX, mapY = GameMap.GetSize()
-    print(AI_SDK.DirectionToDestination(11, 11, 0))
+    pass
 
 
 def Main():
     '''
     每个Step调用一次
     '''
-    x, y, direction = RandomSelect()
-    AI_SDK.MoveByDirection(x, y, 0, direction)
-    print("Python Called")
+    pass
