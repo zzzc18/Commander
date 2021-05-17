4bb2b24ee15d024eca11a41f86781c9464a8aa64
zzzc18 <424729227@qq.com>
2021-05-17 22:44:11 +0800

    减少最外层文件数
    python文件移动到AutoMatch被调用即可，直接调用match.py工作路径算作根目录

--- AutoMatch/MultiTest.py
+++ AutoMatch/MultiTest.py
@@ -1,11 +1,11 @@
 from multiprocessing import Pool
-from autoMatch import autoMatch, Match
+from AutoMatch.autoMatch import autoMatch, Match
 import time
 
 
-if __name__ == '__main__':
+def MultiTest(_processes=1):
     startTime = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
-    with Pool(processes=10) as pool:         # start 4 worker processes
+    with Pool(processes=_processes) as pool:         # start 4 worker processes
         args = []  # [[port,index],...]
         for i in range(100):
             args.append([22122+i, i])
@@ -17,3 +17,7 @@ if __name__ == '__main__':
     am.countMatchResult()
     endTime = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
     print(startTime, endTime)
+
+
+if __name__ == '__main__':
+    MultiTest(_processes=12)
